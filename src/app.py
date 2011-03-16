d693e5f57bd8628901eac651098e2e66bc00c524
Anthony Scopatz <scopatz@gmail.com>
2011-03-16 00:29:53 -0500

    Added app stuff.

---
+++ src/app.py
@@ -0,0 +1,24 @@
+import os
+
+from enthought.traits.api import HasTraits, Str
+from enthought.traits.ui.api import View, Group, HGroup, VGroup, ImageEditor, Item
+
+HOME = os.environ["HOME"]
+RECORD_DIR = os.path.join([HOME, "skype"])
+
+
+class App(HasTraits):
+    record_dir = Str(RECORD_DIR)
+
+    inscight_logo_editor = ImageEditor(image=os.path.join("..", "img", "icons", "InSciGHT.jpg"))
+
+    traits_view = View(Item('inscight_logo', 
+                        editor=inscight_logo_editor, 
+                        show_label=False,
+                        ),
+                  )
+
+
+    #
+    # Default Traits
+    #
