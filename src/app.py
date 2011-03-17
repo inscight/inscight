00fa9fa959dc14ea61036c5a422defc15c645f95
Anthony Scopatz <scopatz@gmail.com>
2011-03-16 19:59:12 -0500

    Added Jeff.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 from enthought.traits.api import HasTraits, Str
 from enthought.traits.ui.api import View, Group, HGroup, VGroup, ImageEditor, Item
@@ -9,6 +10,7 @@ RECORD_DIR = os.path.join([HOME, "skype"])
 
 class App(HasTraits):
     record_dir = Str(RECORD_DIR)
+    ls_record_dir = Str
 
     inscight_logo_editor = ImageEditor(image=os.path.join("..", "img", "icons", "InSciGHT.jpg"))
 
@@ -22,3 +24,7 @@ class App(HasTraits):
     #
     # Default Traits
     #
+
+    def _ls_record_dir_default(self):
+        lrd = subprocess.check_output(['ls', '-l', os.path.join])
+        return lrd
