bd1e49ea3539683225f5a7ec9f64963be36f38f9
Anthony Scopatz <scopatz@gmail.com>
2012-02-19 16:42:54 -0600

    Added initial version of licences.py

--- img/blog/licenses.py
+++ img/blog/licenses.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 
 
-plt.scatter([0.98, 0.02], [0.98, 0.02], 50, plt.cm.jet([0.9, 0.1]))
+plt.scatter([0.98, 0.02, 0.95], 
+            [0.98, 0.02, 0.9], 
+            50, plt.cm.jet([0.9, 0.1]))
 
 plt.text(0.97, 0.97, "WTFPL", ha='right', va='top')
 plt.text(0.03, 0.03, "Windows EULA")
+plt.text(0.97, 0.97, "GPL", ha='right', va='top')
 
 plt.grid(True, linestyle=':')
 plt.axis([0.0, 1.0, 0.0, 1.0])
