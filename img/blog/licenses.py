68b1f66f1d0cf9addfbf42717833fd92cdd67b6c
Anthony Scopatz <scopatz@gmail.com>
2012-03-31 17:06:50 -0500

    Added some images for blog

--- img/blog/licenses.py
+++ img/blog/licenses.py
@@ -1,13 +1,19 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
+x = np.array([0.02, 0.15, 0.5, 0.95, 0.95, 0.85, 0.98])
+y = np.array([0.02, 0.50, 0.7, 0.75, 0.85, 0.95, 0.98])
 
-plt.scatter([0.98, 0.02, 0.95], 
-            [0.98, 0.02, 0.9], 
-            50, plt.cm.jet([0.9, 0.1]))
+plt.scatter(x, y, 50, plt.cm.jet(np.sqrt(x**2 + y**2)))
 
-plt.text(0.97, 0.97, "WTFPL", ha='right', va='top')
-plt.text(0.03, 0.03, "Windows EULA")
-plt.text(0.97, 0.97, "GPL", ha='right', va='top')
+
+plt.text(0.04, 0.02, "Windows EULA", va='center')
+plt.text(0.17, 0.50, "Shareware", va='center')
+plt.text(0.52, 0.70, "Freeware", va='center')
+plt.text(0.94, 0.85, "GPL", ha='right', va='center')
+plt.text(0.83, 0.95, "BSD", ha='right', va='center')
+plt.text(0.93, 0.75, "FLASH", ha='right', va='center')
+plt.text(0.97, 0.97, "WTFPL", ha='right', va='center')
 
 plt.grid(True, linestyle=':')
 plt.axis([0.0, 1.0, 0.0, 1.0])
